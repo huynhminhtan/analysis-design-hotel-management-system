1fe3059fc5bf6c2ad1948701412a391f4addc2b0
mtSiniChi <minhtan.itdev@gmail.com>
2018-05-24 22:47:38 +0700

    - Upload source code simple MVC model - Test link multi project in solution with print comment to console

---
+++ part-hotel-management-system/BUS/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BUS
+{
+    public class Class1
+    {
+        public static void PrintTest()
+        {
+
+            DTO.Class1.PrintTest();
+            DAO.Class1.PrintTest();
+
+            Console.WriteLine("test BUS");
+            return;
+        }
+    }
+}
---
+++ part-hotel-management-system/DAO/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAO
+{
+    public class Class1
+    {
+
+        public static void PrintTest()
+        {
+            Console.WriteLine("test DAO");
+            return;
+        }
+
+    }
+}
